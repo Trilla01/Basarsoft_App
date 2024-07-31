160b8850c761497b7cb99013b7a0a72f85a23089
Yigit <yigit.ozhan@ug.bilkent.edu.tr>
2024-07-31 20:15:04 +0300

    final commit4

--- lib/entrycard.dart
+++ lib/entrycard.dart
@@ -98,38 +98,3 @@ class _EntrycardState extends State<Entrycard> {
   }
 }
 
-
-
-
-
-
-
-   /*@override
- Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(entry.date, style: GoogleFonts.montserrat(fontSize: 18)),
-                  Text((entry.distance / 1000).toStringAsFixed(2) + " km",
-                      style: GoogleFonts.montserrat(fontSize: 18)),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(entry.duration,
-                      style: GoogleFonts.montserrat(fontSize: 14)),
-                  Text(entry.speed.toStringAsFixed(2) + " km/h",
-                      style: GoogleFonts.montserrat(fontSize: 14)),
-                ],
-              )
-            ],
-          )),
-    );
-  }*/
